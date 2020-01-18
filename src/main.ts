49c70f02c8525730a2f6cb87946d1355f4b1b4b9
Savas Vedova <savas@vsk.me>
2020-01-18 22:28:28 +0100

    make is serverless

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (process.env.SK_APP_ID) {
+    await app.init();
+    return app.getHttpAdapter().getInstance();
+  }
+  
   await app.listen(3000);
 }
-bootstrap();
+
+export default bootstrap();
